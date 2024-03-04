b0e2a9ef9db08bad73401a987a3c975b96dba436
sandalisingh <sandalisingh.02@gmail.com>
2024-03-04 13:19:35 +0530

    commit

--- AI/DialogueGenerator/DialogueGenerator_Train.py
+++ AI/DialogueGenerator/DialogueGenerator_Train.py
@@ -1,15 +1,14 @@
 from DialogueGenerator import DialogueGenerator
-import tensorflow as tf
 
 # Instantiate the Chatbot class
 dialogue_generator = DialogueGenerator()
 
 # Define model
 # dialogue_generator.define_model()
-print(dialogue_generator.model)
+# print(dialogue_generator.model)
 
 # Train the model
-# dialogue_generator.create_train_and_save_model()
+dialogue_generator.create_train_and_save_model()
 
 # Inspection
 # dialogue_generator.inspect_layer_outputs()
