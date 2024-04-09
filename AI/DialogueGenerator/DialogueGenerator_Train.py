c146b55760000f8976bf302d63e41ce8e030d486
sandalisingh <sandalisingh.02@gmail.com>
2024-04-09 18:49:48 +0530

    Update

--- AI/DialogueGenerator/DialogueGenerator_Train.py
+++ AI/DialogueGenerator/DialogueGenerator_Train.py
@@ -6,7 +6,7 @@ dialogue_generator = DialogueGenerator()
 # Train the model
 # dialogue_generator.model_visualization()
 
-dialogue_generator.train_and_test('Datasets/Manual.csv', 20)
+dialogue_generator.train_and_test('Datasets/Synthetic-Persona-Chat_train.csv_1.csv', 'Datasets/Synthetic-Persona-Chat_valid.csv', 'Datasets/Synthetic-Persona-Chat_test.csv', 50)
 # print("--------------------------Conversation - Done------------------------------")
 
 # dialogue_generator.train_and_test('Datasets/Dataset_69K_Kaggle_1.csv', 5)
